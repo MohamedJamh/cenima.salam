d20987e1d5d5d2573faa912289327da3e39a8e79
MohamedJamh <jam7.mohamed@gmail.com>
2023-03-06 17:29:36 +0100

    Models and migrations part 1

---
+++ app/Models/Seat.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Seat extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'row_label',
+        'number'
+    ];
+}
---
+++ app/Models/Showtime.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Showtime extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'date',
+        'starts',
+        'ends'
+    ];
+}
