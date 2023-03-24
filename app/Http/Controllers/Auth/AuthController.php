c6f3fca7192a28be6e96644e8f89079f6efae389
MohamedJamh <jam7.mohamed@gmail.com>
2023-03-24 10:28:18 +0000

    adding auth routes, fixing auth controller namespace, verified middleware

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Auth;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Http\Resources\User\UserResource;
